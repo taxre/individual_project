893a257b8e94508ae9a8d39c71d3eb513529701d
neznam <neznamve@abv.bg>
2013-12-11 02:59:48 +0000

    first commit

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Valeri Chakarov"
+    email    "vchakarov@yahoo.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
